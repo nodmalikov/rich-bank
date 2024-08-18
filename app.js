2670c99de916bed1bbd77da1c5944bf292b06df2
nodmalikov <abcmalikov@gmail.com>
2024-08-18 21:19:17 +0500

    Add hero section and make responsive

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const elHamburger = document.querySelector('.hamburger');
 const elSitenav = document.querySelector('.sitenav');
+const elHeader = document.querySelector('.site-header');
+const elMainContent = document.querySelector('.main-content');
+const elSiteHeaderBg = document.querySelector('.site-header__bg');
 
+// HAMBURGER
 elHamburger.addEventListener('click', function () {
     elHamburger.classList.toggle('active');
     elSitenav.classList.toggle('active');
+    elSiteHeaderBg.classList.toggle('active')
 });
+
+// OF MAIN MARGIN TOP
+function headerAutoMargin() {
+    const headerHeight = elHeader.offsetHeight;
+    elMainContent.style.marginTop = `${headerHeight}px`
+};
+
+headerAutoMargin();
+new ResizeObserver(headerAutoMargin).observe(elHeader);
